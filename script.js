eb94606e9210021b7c3e99654cea43c17b51361c
ASN-Jalangi <asn.jalangi@gmail.com>
2024-10-17 17:13:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basic form submission handler
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message. We will get back to you soon!');
+});
