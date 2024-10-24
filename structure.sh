8d4801e219d7e78b0015e8a7fd1879591764444a
ASN-Jalangi <asn.jalangi@gmail.com>
2024-10-24 18:11:15 +0530

    Create structure.sh

---
+++ structure.sh
@@ -0,0 +1,14 @@
+/ASN
+  ├── index.html
+  ├── about.html
+  ├── gallery.html
+  ├── contact.html
+  ├── admin.html
+  ├── style.css
+  ├── script.js
+  ├── images/
+      ├── ASN.png
+      ├── ASN_Cover.jpg
+  ├── notices/
+      ├── notice1.pdf
+      ├── notice2.pdf
